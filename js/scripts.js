63d9e478f43d1fae92498c3fca85aa33f5832a81
abuckenberger <abuckenberger@eagles.ewu.edu>
2017-03-09 18:26:31 -0800

    RANDOM BGCOLOR
    *added array of random background colors
    *click event only working once?
    *trying to figure it out

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+//random number generator
+var maxRand = 15;
+var numRand = Math.floor( Math.random() * maxRand );
+console.log(numRand);
+
+//background colors
+var bgcolor = [ '#c3ad91', '#c3ad91', '#cbbcaa', '#f5e7d5', '#c7a983', '#d9cec1', '#efe7dc', '#77644c', '#5a4b39', '#fff3e4', '#5d482d', '#2a231a', '#cb9d64', '#f7eee4', '#efc188']
+
+console.log(bgcolor[numRand]);
+
+//store color change function
+
+var colorchange = $('body').css('background-color', bgcolor[numRand]);
+
 // YOUR SCRIPTS GO HERE
 // ACTIVATE GALLERY
 
@@ -8,10 +22,26 @@ $('figure').lightGallery({
     
 });
 
+//On h1 click the content appears
+
 $('h1').click(function(){
     
  $('figure').fadeIn('1s'); 
   
 });
+
+
+//On each h1 click the background changes 
+
+
+
+
+
+
+
+
+
+
+
 
 
